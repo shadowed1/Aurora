f974f34c098869c76c5612a5b12e9517105526fb
shadowed1 <itszachg@gmail.com>
2025-06-24 19:45:21 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -54,6 +54,8 @@ download_and_extract()
 
     echo "${MAGENTA}${FILE} extracted.${RESET}"
     export LD_LIBRARY_PATH="$target_dir/usr/lib:$LD_LIBRARY_PATH"
+    export FLATPAK_USER_DIR="$HOME/.local/share/flatpak"
+    LD_LIBRARY_PATH="$HOME/opt/flatpak-deps/usr/lib" ~/opt/flatpak/usr/bin/flatpak --version
     sleep 1
 }
 
@@ -84,6 +86,13 @@ download_and_extract "$URL" "$HOME/opt/flatpak-deps"
 URL="https://archlinux.org/packages/extra/x86_64/xdg-dbus-proxy/download"
 download_and_extract "$URL" "$HOME/opt/flatpak-deps"
 
+sleep 1
+export FLATPAK_USER_DIR="$HOME/.local/share/flatpak"
+LD_LIBRARY_PATH="$HOME/opt/flatpak-deps/usr/lib" ~/opt/flatpak/usr/bin/flatpak --version
+
+sleep 1
+
+echo "Adding Flathub repository"
 mkdir -p ~/tmp
 mkdir -p $HOME/tmp
 TMPDIR=$HOME/tmp
@@ -94,7 +103,51 @@ flatpak --user install flathub org.gnome.gedit
 chown -R $USER:$USER ~/.local/share/flatpak
 chmod -R u+rw ~/.local/share/flatpak
 TMPDIR="$HOME/tmp" flatpak --user install flathub org.gnome.gedit
+sleep 1
 
+export PATH="$HOME/opt/flatpak/usr/bin:$PATH"
+export LD_LIBRARY_PATH="$HOME/opt/flatpak-deps/usr/lib:$LD_LIBRARY_PATH"
+export PATH="$HOME/opt/flatpak-deps/usr/bin:$PATH"
+TMPDIR=$HOME/tmp 
+export TMPDIR="$HOME/tmp"
+export DISPLAY=:0
+export XDG_RUNTIME_DIR="$HOME/.xdg-runtime-dir"
+mkdir -p "$XDG_RUNTIME_DIR"
+chmod 700 "$XDG_RUNTIME_DIR"
+echo "$DBUS_SESSION_BUS_ADDRESS"
+
+
+export DBUS_SESSION_BUS_ADDRESS=$(cat "$XDG_RUNTIME_DIR/dbus-session.address")
+
+export XDG_RUNTIME_DIR="$HOME/.xdg-runtime-dir"
+mkdir -p "$XDG_RUNTIME_DIR"
+chmod 700 "$XDG_RUNTIME_DIR"
+
+if [ ! -f "$XDG_RUNTIME_DIR/dbus-session.address" ]; then
+  dbus-daemon --session \
+    --address="unix:path=$XDG_RUNTIME_DIR/dbus-session" \
+    --print-address=1 \
+    --nopidfile \
+    --nofork > "$XDG_RUNTIME_DIR/dbus-session.address" &
+  sleep 1
+fi
+
+export DBUS_SESSION_BUS_ADDRESS=$(cat "$XDG_RUNTIME_DIR/dbus-session.address")
+
+echo 3 > "$XDG_RUNTIME_DIR/doc/portal/version"
+mkdir -p "$XDG_RUNTIME_DIR/doc/portal"
+chmod 700 "$XDG_RUNTIME_DIR/doc" "$XDG_RUNTIME_DIR/doc/portal"
+mkdir -p "$XDG_RUNTIME_DIR/doc"
+chmod 700 "$XDG_RUNTIME_DIR/doc"
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share:/usr/local/share:/usr/share"
+export GTK_USE_PORTAL=0
+export FLATPAK_DISABLE_PORTAL=1
+
+if ! dbus-send --session --dest=org.freedesktop.DBus --type=method_call \
+     --print-reply /org/freedesktop/DBus org.freedesktop.DBus.ListNames > /dev/null 2>&1; then
+    eval "$(dbus-launch --sh-syntax)"
+    export DBUS_SESSION_BUS_ADDRESS
+fi
         ;;
 exit 0
         ;;
