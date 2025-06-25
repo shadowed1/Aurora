7881ef7ef8337e3bb0673bf9830e9a17e2ab72b9
shadowed1 <itszachg@gmail.com>
2025-06-25 02:38:27 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -21,7 +21,7 @@ echo ""
 echo "${RESET}${CYAN}Run apps on Borealis using Flatpak with signficantly more performance than Crostini!${RESET}${MAGENTA}"
 echo "${RESET}"
 echo "${BLUE}${BOLD}0: Quit${RESET}"
-echo "${MAGENTA}${BOLD}1: Download and install Aurora + Flatpak to ~/opt${RESET}"
+echo "${MAGENTA}${BOLD}1: Download and install Aurora + Flatpak to ~/ and ~/opt${RESET}"
 
 read -rp "Enter (0-1): " choice
 export PATH="$HOME/opt/flatpak/usr/bin:$HOME/opt/flatpak-deps/usr/bin:$HOME:$PATH"
