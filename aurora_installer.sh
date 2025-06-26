8b154b1151444d0c6033a4bf4d59b8d66c0fe4ab
shadowed1 <itszachg@gmail.com>
2025-06-25 23:59:43 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -169,6 +169,29 @@ fi
 "$HOME/opt/flatpak/usr/bin/flatpak" --version
 sleep 3
 
+PORTAL_ENV_BLOCK='
+# ----- Aurora Flatpak Borealis Portal & D-Bus env setup -----
+export XDG_RUNTIME_DIR="$HOME/.xdg-runtime-dir"
+mkdir -p "$XDG_RUNTIME_DIR/doc/portal"
+chmod 700 "$XDG_RUNTIME_DIR" "$XDG_RUNTIME_DIR/doc" "$XDG_RUNTIME_DIR/doc/portal"
+echo 3 > "$XDG_RUNTIME_DIR/doc/portal/version"
+
+if [ ! -f "$XDG_RUNTIME_DIR/dbus-session.address" ]; then
+  dbus-daemon --session \
+    --address="unix:path=$XDG_RUNTIME_DIR/dbus-session" \
+    --print-address=1 \
+    --nopidfile \
+    --nofork > "$XDG_RUNTIME_DIR/dbus-session.address" &
+  sleep 1
+fi
+export DBUS_SESSION_BUS_ADDRESS=$(cat "$XDG_RUNTIME_DIR/dbus-session.address")
+# ------------------------------------------------------------
+'
+
+if ! grep -q 'Aurora Flatpak Borealis Portal & D-Bus env setup' "$HOME/.bashrc"; then
+  echo "$PORTAL_ENV_BLOCK" >> "$HOME/.bashrc"
+fi
+
 /bin/bash ~/opt/bin/aurora help
 
 sleep 3
