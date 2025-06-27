4965a489ee626b6cf2bde89938474a1ac1974436
shadowed1 <itszachg@gmail.com>
2025-06-27 09:01:34 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -128,10 +128,6 @@ download_and_extract "$URL" "$HOME/opt/flatpak-deps"
 URL="https://archlinux.org/packages/extra/x86_64/xdg-desktop-portal-gtk/download"
 download_and_extract "$URL" "$HOME/opt/flatpak-deps"
 
-# Futile attempt 
-URL="https://archlinux.org/packages/extra/x86_64/fuse3/download"
-download_and_extract "$URL" "$HOME/opt/flatpak-deps"
-
 curl -L https://raw.githubusercontent.com/shadowed1/Aurora/main/flatpak.logic -o ~/opt/flatpak.logic
 curl -L https://raw.githubusercontent.com/shadowed1/Aurora/main/aurora -o ~/opt/bin/aurora
 curl -L https://raw.githubusercontent.com/shadowed1/Aurora/main/flatpak.env -o ~/opt/flatpak.env
