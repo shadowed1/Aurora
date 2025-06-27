d201c8cc8b7e9063dc64e235f628d89381bd7c6f
shadowed1 <itszachg@gmail.com>
2025-06-26 21:04:45 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -174,8 +174,8 @@ sleep 3
 sleep 3
 echo "${MAGENTA}"
 echo "╔═══════════════════════════════════════════════════════════════════════════════════════════════╗"
-echo "║                                       ${RESET}${BOLD}${MAGENTA}DOWNLOAD COMPLETE!${RESET}${MAGENTA}                                      ║"
-echo "║           Open a new Crosh tab and run ${RESET}${BOLD}${MAGENTA}vsh borealis${RESET}${MAGENTA} to continue setting up Flatpak            ║"
+echo "║                                       ${RESET}${BOLD}${MAGENTA}DOWNLOAD COMPLETE!${RESET}${BLUE}${BOLD}                                      ║"
+echo "║           Open a new Crosh tab and run ${RESET}${BOLD}${CYAN}vsh borealis${RESET}${BLUE}${BOLD} to continue setting up Flatpak${RESET}${MAGENTA}            ║"
 echo "╚═══════════════════════════════════════════════════════════════════════════════════════════════╝"
 echo "${RESET}"
 echo ""
