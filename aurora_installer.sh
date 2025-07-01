7baab09a4b0684efa849000edc3ba0470fea1e28
shadowed1 <itszachg@gmail.com>
2025-07-01 12:29:47 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -175,6 +175,9 @@ download_and_extract "$URL" "$HOME/opt/"
 URL="https://archlinux.org/packages/extra/x86_64/zlib-ng/download"
 download_and_extract "$URL" "$HOME/opt/"
 
+URL="https://archlinux.org/packages/core/x86_64/libcurl-gnutls/download"
+download_and_extract "$URL" "$HOME/opt/"
+
 
 curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/.flatpak.logic -o ~/opt/.flatpak.logic
 curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/aurora -o ~/opt/bin/aurora
