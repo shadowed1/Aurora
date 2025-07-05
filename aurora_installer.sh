eed59d11a3fb8fe639967a4abb7d88007c32a4f5
shadowed1 <itszachg@gmail.com>
2025-07-04 20:20:39 -0400

    Update aurora_installer.sh

--- aurora_installer.sh
+++ aurora_installer.sh
@@ -550,6 +550,12 @@ download_and_extract "$URL" "$HOME/opt/"
 URL="https://archlinux.org/packages/extra/x86_64/libxcb/download"
 download_and_extract "$URL" "$HOME/opt/"
 
+URL="https://archlinux.org/packages/extra/x86_64/xcb-util-renderutil/download"
+download_and_extract "$URL" "$HOME/opt/"
+URL="https://archlinux.org/packages/extra/any/xorgproto/download"
+download_and_extract "$URL" "$HOME/opt/"
+URL="https://archlinux.org/packages/extra/any/xcb-proto/download"
+download_and_extract "$URL" "$HOME/opt/"
 
 curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/.flatpak.logic -o ~/opt/.flatpak.logic
 curl -L https://raw.githubusercontent.com/shadowed1/Aurora/beta/aurora -o ~/opt/bin/aurora
